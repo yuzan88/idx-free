c2e579d5e0d14d50c87d2f01e1c2ba150395c1a0
yuzan88 <leebgaio@gmail.com>
2026-02-17 23:19:00 +0800

    Update clnlinstall.sh

--- clnlinstall.sh
+++ clnlinstall.sh
@@ -28,4 +28,4 @@ export UPLOAD_URL=              # 节点自动推送到订阅器，需要填写�
 
 # --- 执行主部署脚本 ---
 # 这会下载并执行远程的 sb.sh 脚本，并使用上面设置的环境变量
-bash <(curl -Ls https://main.ssss.nyc.mn/sb.sh)
+bash <(curl -Ls https://xgp.eooce.com/sb.sh)
